a24503277f415c325da39a68eb55753072b37a1c
houxuefeng <m18821753787_1@163.com>
2017-06-22 10:11:39 +0800

    日期格式转化

--- src/main/java/cn/xapi/hou/controller/BookCo.java
+++ src/main/java/cn/xapi/hou/controller/BookCo.java
@@ -79,6 +79,8 @@ public class BookCo {
 		Map<String, Object> map2 = new HashMap<>();
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 		String currentDate = format.format(new Date());
+		
+		System.out.println(currentDate+"------------->");
 		// 预约库存-1
 		map.put("booknumber", 0);
 		map.put("bookno", orderbookno);
@@ -92,6 +94,7 @@ public class BookCo {
 		map2.put("yuyueUserNo", record.getReaderno());
 
 		map2.put("intime", currentDate);
+		
 		map2.put("outtime", currentDate);
 
 		if (book.selectBookNumber(orderbookno) < 1) {
